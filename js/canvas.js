fdf67a30879d5667a08c525e70e1ab3fe8f565be
BeaVix <98414235+BeaVix@users.noreply.github.com>
2026-08-12 17:37:10 -0300

    got rid of debug console.logs

--- js/canvas.js
+++ js/canvas.js
@@ -82,7 +82,6 @@ class Canvas{
             this.drawQueue.sort((a,b) => {
             if(a instanceof Player && !(b instanceof Player)){
                 return (a.movementComponent.pos[1] + a.animationComponent.size[1]*2) - (b.data.pos[1]+ b.data.size[1] )*b.data.scale 
-                console.log(b.data.pos[1])
             }else if( b instanceof Player && !(a instanceof Player)){
                 return ( a.data.pos[1] + a.data.size[1])*a.data.scale - (b.movementComponent.pos[1] + b.animationComponent.size[1]*2)
             }else if (a instanceof Player && b instanceof Player){
@@ -91,8 +90,6 @@ class Canvas{
                 return  a.data.pos[1]*a.data.scale - b.data.pos[1]*b.data.scale
             }
         })
-
-        console.log(players[0].movementComponent.pos[1])
 
         //Draw objects in the queue
         this.drawQueue.forEach(object => {
